98b0679846784ef9822a61ce5f1c88098b7e63fa
Bruno Marques <besmarques@gmail.com>
2022-01-03 21:18:27 +0000

    Idea but wrong

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const symbol = ["&#9824;", "&#9829", "&#9830;", "&#9827;"];
+const value = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let htmlSymbol = Math.floor(Math.random() * 4);
+  let htmlValue = Math.floor(Math.random() * value.length);
+  for (let i = 0; i < document.getElementsByClassName("symbol").length; i++) {
+    document.getElementsByClassName("symbol")[i].innerHTML = symbol[htmlSymbol];
+    if (
+      document.getElementsByClassName("symbol")[i].innerHTML == "♥" ||
+      document.getElementsByClassName("symbol")[i].innerHTML == "♦"
+    ) {
+      document.getElementsByClassName("symbol")[i].style.color = "red";
+    }
+  }
+  for (let z = 0; z < document.getElementsByClassName("values").length; z++) {
+    document.getElementsByClassName("values")[z].innerHTML = value[htmlValue];
+  }
 };
