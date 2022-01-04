edd312dad7ebba71244dc21d0b80492254fbc5ff
Bruno Marques <besmarques@gmail.com>
2022-01-04 13:19:22 +0000

    first step done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-const symbol = ["&#9824;", "&#9829", "&#9830;", "&#9827;"];
+const suits = ["hearts", "spades", "clubs", "diamonds"];
+const symbol = ["&#9829;", "&#9824;", "&#9827;", "&#9830;"];
 const value = [
   "A",
   "2",
@@ -24,16 +25,27 @@ const value = [
 
 window.onload = function() {
   //write your code here
-  let htmlSymbol = Math.floor(Math.random() * 4);
+
+  let suitGenerated = suits[Math.floor(Math.random() * 4)];
+  console.log(suitGenerated);
+
+  document.getElementsByClassName("card")[0].classList =
+    "card " + suitGenerated + " col-3 mt-5 mb-5 p-2";
+  let htmlSymbol = "";
+
+  if (suitGenerated == "hearts") {
+    htmlSymbol = 0;
+  } else if (suitGenerated == "spades") {
+    htmlSymbol = 1;
+  } else if (suitGenerated == "clubs") {
+    htmlSymbol = 2;
+  } else {
+    htmlSymbol = 3;
+  }
+
   let htmlValue = Math.floor(Math.random() * value.length);
   for (let i = 0; i < document.getElementsByClassName("symbol").length; i++) {
     document.getElementsByClassName("symbol")[i].innerHTML = symbol[htmlSymbol];
-    if (
-      document.getElementsByClassName("symbol")[i].innerHTML == "♥" ||
-      document.getElementsByClassName("symbol")[i].innerHTML == "♦"
-    ) {
-      document.getElementsByClassName("symbol")[i].style.color = "red";
-    }
   }
   for (let z = 0; z < document.getElementsByClassName("values").length; z++) {
     document.getElementsByClassName("values")[z].innerHTML = value[htmlValue];
